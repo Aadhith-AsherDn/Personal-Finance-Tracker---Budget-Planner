41dda9c9b895786e53c25cbe9ffcf1605fb2f3c9
Aadhith <aadhith@asherdn.com>
2026-06-18 11:26:52 +0530

    resloved password error and user id error

--- Backend/project/controllers/user-control.js
+++ Backend/project/controllers/user-control.js
@@ -5,12 +5,47 @@ const bcrypt = require("bcrypt");
 
 const register = asyncHandler(async (req, res) => {
     const { userName, userEmail, userPassword } = req.body;
-
     if (!userName || !userEmail || !userPassword) {
         return res.status(400).json({
             msg: "Fill all details"
         });
     }
+    const passwordlength = userPassword.length;
+
+    if (passwordlength < 8) {
+    return res.status(400).json({
+        msg: "Password must be at least 8 characters long"
+    });
+}
+
+    let uppercase = 0;
+    let lowercase = 0;
+    let splchar = 0;
+    let num = 0;
+
+    for (let i = 0; i < passwordlength; i++) {
+        const hexcode = userPassword.charCodeAt(i);
+
+        if (
+            (hexcode >= 0x20 && hexcode <= 0x27) ||
+            (hexcode >= 0x3B && hexcode <= 0x40) ||
+            (hexcode >= 0x5B && hexcode <= 0x60) ||
+            (hexcode >= 0x7B && hexcode <= 0x7E)
+        ) splchar++;
+
+        if (hexcode >= 0x41 && hexcode <= 0x5A) uppercase++;
+
+        if (hexcode >= 0x61 && hexcode <= 0x7A) lowercase++;
+
+        if (hexcode >= 0x30 && hexcode <= 0x39) num++;
+    }
+
+    if (uppercase === 0 || lowercase === 0 || splchar === 0 || num === 0) {
+    return res.status(400).json({
+        msg: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    });
+}
+    
 
     const oldUser = await login.findOne({ userEmail });
 
@@ -21,8 +56,11 @@ const register = asyncHandler(async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(userPassword, 10);
+    const lastUser = await login.findOne().sort({ userId: -1 });
+    const nextUserId = lastUser ? lastUser.userId + 1 : 1;
 
     const newUser = await login.create({
+        userId: nextUserId,
         userName,
         userEmail,
         userPassword: hashedPassword,
@@ -30,7 +68,7 @@ const register = asyncHandler(async (req, res) => {
 
     return res.status(201).json({
         msg: "Registered successfully",
-        userId: newUser._id
+        userId: nextUserId,
     });
 });
 
--- Backend/project/models/login-model.js
+++ Backend/project/models/login-model.js
@@ -1,37 +1,29 @@
 const mongoose = require("mongoose");
 
-const AutoIncrement = require("mongoose-sequence")(mongoose);
-
 const loginSchema = new mongoose.Schema({
-     userName: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
+    userId: {
+        type: Number,
+        unique: true
     },
-
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
     userEmail: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
     },
-
     userPassword: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function(value) {
-          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
-        },
-        message: 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
-      }
-    },
-},
-{
-    timestamps:true
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
 });
-loginSchema.plugin(AutoIncrement, { inc_field: 'userId' });
 
-module.exports = mongoose.model("login",loginSchema);
+module.exports = mongoose.model("login", loginSchema);
